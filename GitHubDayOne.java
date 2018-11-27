5652d3df7ab9b9029e491443938fc81c73dc325b
gsakul21 <gsakul21@yahoo.com>
2018-11-27 12:27:06 -0500

    Added Combine Method
    Added method that intakes two ArrayLists and returns a new ArrayList with both the intake lists within it.

--- GitHubDayOne.java
+++ GitHubDayOne.java
@@ -8,7 +8,18 @@ public class GitHubDayOne{
 	}
 
 
+public static ArrayList<Integer> Combine(ArrayList<Integer> a, ArrayList<Integer> b){
+	ArrayList<Integer> c = new ArrayList<>();
+	for(int x = 0; x < a.size(); x++)
+		c.add(x, a.get(x));
 
+	for(int x = 0; x < b.size(); x++)
+		c.add((a.size() + x), b.get(x));
+
+		return c;
+
+
+}
 
 
 
